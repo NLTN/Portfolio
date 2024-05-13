f68d0050f315a83a0220fc531d4d75a943c9c9ec
Nguyen Nguyen <nltn@msn.com>
2024-05-12 23:27:28 -0700

    Added some basic components

---
+++ src/components/About/About.jsx
@@ -0,0 +1,18 @@
+import "./About.css";
+import React from "react";
+import { Container } from "react-bootstrap";
+function template() {
+  return (
+    <Container>
+    <h1 style={{backgroundColor: 'red'}}>About</h1>
+
+    <p>
+      dsadsad
+      dsasasd
+      dsasadsa
+    </p>
+  </Container>
+  );
+};
+
+export default template;
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,26 @@
+import "./Hero.css";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image'
+
+function template() {
+  return (
+    <Container className="hero">
+      <Row>
+        <Col>
+          <Image roundedCircle src="https://avatars.githubusercontent.com/u/9380605?s=400&u=f18130f9a63a1c21eba83f8608479033d7a55c21&v=4" />
+        </Col>
+        <Col>
+          <h1>Nathan <span style={{color: "skyblue"}}>Nguyen</span></h1>
+          <p>
+            Software Engineer based in California
+          </p>
+        </Col>
+      </Row>
+    </Container>
+
+  );
+};
+
+export default template;
