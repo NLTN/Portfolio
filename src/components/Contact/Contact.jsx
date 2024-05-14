a446b64eb40b643c1cc0ac19b041c83ae399a7fb
Nguyen Nguyen <nltn@msn.com>
2024-05-14 03:22:38 -0700

    Added contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,42 +5,60 @@ import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import { Container } from "react-bootstrap";
+import Form from 'react-bootstrap/Form';
 
 function template() {
   return (
     <Container data-aos="fade-up" className="mt-5 mb-4">
-      <h1 id="contact" className="projects pb-4 ">Contact</h1>
+      <h1 id="contact" className="pb-4 ">Contact</h1>
       <h4>I want to hear from you!</h4>
 
-      <Row xs={1} md={3} className="g-4">
-        <Col>
-          <Card className="bg-transparent" border="white">
+      <Row xs={1} md={2} className="g-4">
+        <Col xs={12} md={4}>
+          <Card className="contact-card bg-transparent" border="white">
             <Card.Body>
-              <Card.Title><i className="fa fa-linkedin" style={{ color: "white", fontSize: "2em" }}></i></Card.Title>
               <Card.Text>
+                <i className="fa fa-linkedin"></i>
                 <Button variant="link" className="text-white" href="https://linkedin.com/in/nltn/">linkedin.com/NLTN</Button>
+              </Card.Text>
+              <Card.Text>
+                <i className="fa fa-github"></i>
+                <Button variant="link" href="https://github.com/NLTN">github.com/NLTN</Button>
+              </Card.Text>
+              <Card.Text>
+                <i className="fa fa-envelope"></i>
+                <Button variant="link" href="mailto: nathan@me.com">nathan@me.com</Button>
               </Card.Text>
             </Card.Body>
           </Card>
 
         </Col>
-        <Col>
-          <Card className="bg-transparent contact-card" border="white">
+        <Col xs={12} md={8}>
+          <Card className="bg-transparent" border="white">
             <Card.Body>
-              <Card.Title><i className="fa fa-github"></i></Card.Title>
-              <Card.Text>
-                <Button variant="link" href="https://github.com/NLTN">github.com/NLTN</Button>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card className="bg-transparent contact-card" border="white">
-            <Card.Body>
-              <Card.Title><i className="fa fa-envelope"></i></Card.Title>
-              <Card.Text>
-                nathan@me.com
-              </Card.Text>
+              <Form>
+                <Row className="mb-3">
+                  <Col>
+                    <Form.Group controlId="textbox1">
+                      <Form.Control type="text" placeholder="Your name" />
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <Form.Group controlId="textbox2">
+                      <Form.Control type="email" placeholder="Your email" />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                  <Form.Control type="text" placeholder="Subject" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                  <Form.Control as="textarea" rows={3} placeholder="Write your message here" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Submit now
+                </Button>
+              </Form>
             </Card.Body>
           </Card>
         </Col>
