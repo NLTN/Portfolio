284675cbc4c8e9c2c40eb389c66561b3887c1659
Nguyen Nguyen <nltn@msn.com>
2024-05-15 01:03:26 -0700

    Updated image size

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,22 +2,23 @@ import "./Hero.css";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image'
+import { Container } from "react-bootstrap";
 
 function template() {
   return (
-      <div id="home" className="hero">
-      <Row  xs={1} md={2} className="align-items-center" >
+    <Container id="home" className="hero">
+      <Row xs={1} md={2} className="align-items-center" >
         <Col>
           <Image roundedCircle className="mob-img" src="https://avatars.githubusercontent.com/u/9380605?s=400&u=f18130f9a63a1c21eba83f8608479033d7a55c21&v=4" />
         </Col>
         <Col>
-          <h1>Nathan <span style={{color: "skyblue"}}>Nguyen</span></h1>
+          <h1>Nathan <span className="lastname">Nguyen</span></h1>
           <p className="subtitle">
             Software Engineer based in California
           </p>
         </Col>
       </Row>
-      </div>
+    </Container>
 
   );
 };
