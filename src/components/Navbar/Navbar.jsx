8d73b59c51c6a1ef26e44373dee0961084604529
Nguyen Nguyen <nltn@msn.com>
2024-05-15 01:01:50 -0700

    Changed expand prop in the Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,19 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import "./Navbar.css";
 import React from "react";
 
 function template() {
   return (
-    <Navbar fixed="top" expand="lg" className="navbar-dark	 bg-dark">
+    <Navbar fixed="top" expand="sm" className="navbar-dark bg-dark">
       <Container>
-        {/* <Navbar.Brand href="#home">NN</Navbar.Brand> */}
+        <Navbar.Brand href="#home"></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav " />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
+            {/* <Nav.Link href="#about">About</Nav.Link> */}
             <Nav.Link href="#projects">Projects</Nav.Link>
             <Nav.Link href="#resume">Resume</Nav.Link>
             <Nav.Link href="#contact">Contact</Nav.Link>
